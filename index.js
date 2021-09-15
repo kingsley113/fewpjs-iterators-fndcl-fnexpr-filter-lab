19ef25455aab47150c666d6c7048f1ede2c8eb32
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-09-14 19:25:50 -0700

    commit tree def1449c5962f5a3ee0b49925668739ee2b9ac48

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+// Matching
+function findMatching(drivers, name) {
+  let matches = drivers.filter((driver) => {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+  return matches;
+}
+
+// Fuzzy Finder
+function fuzzyMatch(drivers, search) {
+  let matches = drivers.filter((driver) => {
+    return driver.slice(0, search.length) === search;
+  });
+  return matches;
+}
+
+// Match Name
+function matchName(drivers, name) {
+  let matches = drivers.filter((driver) => {
+    return driver.name === name;
+  });
+  return matches;
+}
